b37612af91960a6afe71cb0dce11988b906cdaa3
Vasi97 <vasistha97@gmail.com>
2025-08-07 20:38:52 +0530

    Add to cart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -46,7 +46,7 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id = "${product.id}">
             Add to Cart
           </button>
         </div>
@@ -54,10 +54,43 @@ products.forEach((product) => {
 
 });
 
-console.log(productsHTML);
-
 document.querySelector('.js-products-grid')
   .innerHTML = productsHTML;
 
-// document.querySelector('.js-products-grid')
-//   .innerHTML = products;
+document.querySelectorAll('.js-add-to-cart')
+  .forEach((button) => {
+    button.addEventListener('click', () => {
+      const productId = button.dataset.productId;
+
+      let matchinItem;
+
+      cart.forEach((item) => {
+        if (productId === item.productId) {
+          matchinItem = item;
+        }
+      });
+
+      if (matchinItem) {
+        matchinItem.quantity += 1;
+      } else {
+         cart.push({
+        productId: productId,
+        quantity: 1
+      });
+      }
+
+      let cartQuantity = 0;
+      
+      cart.forEach((item) =>{
+        cartQuantity += item.quantity;
+      })
+      
+      
+      document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
+      
+        console.log(cartQuantity);
+      console.log(cart);     
+    });
+  });
+
